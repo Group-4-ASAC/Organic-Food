2b3ed9585351c914ca0b3a90f4893a3a9c0ea902
moetaji <mohmmad.altaji@gmail.com>
2021-04-18 21:35:48 +0300

    staching

--- js/products.js
+++ js/products.js
@@ -15,92 +15,87 @@ function Products(name, link, price, description) {
 }
 
 
-let apple= new Products('apple','https://cdn.pixabay.com/photo/2017/09/26/13/21/apple-2788599__340.jpg',12,'vitminC');
+let apple = new Products('apple', 'https://cdn.pixabay.com/photo/2017/09/26/13/21/apple-2788599__340.jpg', 12, 'vitminC');
 
 
 
 let form = document.getElementById("inputForm");
 form.addEventListener('submit', submitter);
-function submitter(event){
+function submitter(event) {
     event.preventDefault();
 
     let name = event.target.name.value;
     let link = event.target.link.value;
     let price = event.target.price.value;
     let description = event.target.description.value;
-    
-    let newProduct= new Products(name, link, price, description);
-    ulElement.textContent="";
-        for(let j=0; j<allProducts.length;j++)
-    {
+
+    let newProduct = new Products(name, link, price, description);
+    ulElement.textContent = "";
+    for (let j = 0; j < allProducts.length; j++) {
         allProducts[j].render();
         console.log("helko");
     }
-    dataStorage();    
+    dataStorage();
 }
 
 
 Products.prototype.render = function () {
-    
+
     let parent = document.getElementById('parent');
-     ulElement = document.createElement('ul');
+    ulElement = document.createElement('ul');
     parent.appendChild(ulElement);
-   
-        let liElement = document.createElement('li');
-        ulElement.appendChild(liElement);
-       
-        let imgElemnt=document.createElement('img');
-        let nameElement=document.createElement('h2');
-        let priceElement=document.createElement('p');
-        let descElement=document.createElement('p');
-        liElement.appendChild(imgElemnt);
-        liElement.appendChild(nameElement);
-        liElement.appendChild(priceElement);
-        liElement.appendChild(descElement);
-        imgElemnt.src=this.link;
-       nameElement.textContent=this.name;
-       priceElement.textContent=this.price;
-       descElement.textContent=this.description;
-       
-        
-              
+
+    let liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+
+    let imgElemnt = document.createElement('img');
+    let nameElement = document.createElement('h2');
+    let priceElement = document.createElement('p');
+    let descElement = document.createElement('p');
+    liElement.appendChild(imgElemnt);
+    liElement.appendChild(nameElement);
+    liElement.appendChild(priceElement);
+    liElement.appendChild(descElement);
+    imgElemnt.src = this.link;
+    nameElement.textContent = this.name;
+    priceElement.textContent = this.price;
+    descElement.textContent = this.description;
+
+
+
 }
-apple.render(); 
-let dataArr=[];
-function dataStorage(){
-let dataArr = JSON.stringify(allProducts);
-localStorage.setItem('product',dataArr);
+apple.render();
+let dataArr = [];
+function dataStorage() {
+    let dataArr = JSON.stringify(allProducts);
+    localStorage.setItem('product', dataArr);
 }
 
-function getDataStorage()
-{
-    let data=localStorage.getItem('product');
-    let productData=JSON.parse(data);
-    if (productData!==null)
-    {
-      let prod;
+function getDataStorage() {
+    let data = localStorage.getItem('product');
+    let productData = JSON.parse(data);
+    if (productData !== null) {
+        let prod;
         //reinstantiation
-        allProducts=[];
-        for(let i=0; i<productData.length; i++)
-        {
-          prod = new Products(productData[i].name,productData[i].link,productData[i].price,productData[i].description); 
+        allProducts = [];
+        for (let i = 0; i < productData.length; i++) {
+            prod = new Products(productData[i].name, productData[i].link, productData[i].price, productData[i].description);
         }
-        ulElement.textContent='';
-       // allProducts[0].render();
+        ulElement.textContent = '';
+        // allProducts[0].render();
         console.log(allProducts[0]);
-        for(let j=0; j<allProducts.length;j++)
-        {
+        for (let j = 0; j < allProducts.length; j++) {
             allProducts[j].render();
             console.log("helko");
         }
         console.log(prod);
         console.log(allProducts);
-      //  allProducts=productData;
-         
+        //  allProducts=productData;
+
         // console.log(productData.length);
         // console.log(allProducts);
     }
 }
-getDataStorage(); 
+getDataStorage();
 
 
