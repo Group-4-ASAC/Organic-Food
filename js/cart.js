f3d5fd5b95e9833cb5daafd357bebc4a9579946a
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-04-19 11:35:29 +0300

    finished cart part expect render

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,51 @@
 "use strict";
-let productArry = ['bnana',2,30];
+let addedToCart=[];
+
+function getAdded() {
+  let added = localStorage.getItem('userCart');
+  let addedProducts =JSON.parse(added);
+  if (addedProducts!==null) {
+    
+    addedToCart=addedProducts;
+  }
+}
+getAdded();
+
+
+let totalPrice = 0;
+let totalCount=0;
+
+console.log(addedToCart);
+for (let j = 0; j < addedToCart.length; j++) {
+  
 let parent = document.getElementById("formCart");
+let labelElement=document.createElement('label');
+labelElement.textContent='Quantity :';
+labelElement.for='userAdded';
+parent.appendChild(labelElement);
+let inputElement=document.createElement('input');
+inputElement.type = 'number';
+inputElement.id = 'userAdded';
+inputElement.name = 'userAdded';
+parent.appendChild(inputElement);
+let submitInput=document.createElement('input');
+submitInput.type='submit';
+parent.appendChild(submitInput);
+
 
 formCart.addEventListener("submit", quntitiySub);
 function quntitiySub(event) {
   event.preventDefault();
-  let quntityProduct = parseInt(event.target.quantity.value);
-  console.log(quntityProduct);
-  for (let i = 0; i < productArry.length; i++) {
-      let inputElement=document.createElement('input');
-      parent.appendChild();
-    totalPrice += productArry[i].price;
+  let quntityProduct = event.target.userAdded.value;
+  // console.log(quntityProduct);
+  for (let i = 0; i < addedToCart.length; i++) {
+    totalCount=0;
+    totalCount= totalCount+quntityProduct;
+    totalPrice += addedToCart[i].price * quntityProduct;
+    console.log(totalCount);
+    console.log(totalPrice);
+
   }
 }
-let totalPrice = 0;
+
+}
