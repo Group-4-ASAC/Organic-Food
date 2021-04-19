0f2b8de38ec36d7ce7caba575b49e6841f990797
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-04-19 11:51:35 +0300

    mergin problem

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,15 @@
-"use strict";
-let productArry = ['bnana',2,30];
-let parent = document.getElementById("formCart");
+'use strict';
+let productArry = ['bnana', 2, 30];
+let parent = document.getElementById('formCart');
 
-formCart.addEventListener("submit", quntitiySub);
+formCart.addEventListener('submit', quntitiySub);
 function quntitiySub(event) {
   event.preventDefault();
   let quntityProduct = parseInt(event.target.quantity.value);
   console.log(quntityProduct);
   for (let i = 0; i < productArry.length; i++) {
-      let inputElement=document.createElement('input');
-      parent.appendChild();
+    let inputElement = document.createElement('input');
+    parent.appendChild();
     totalPrice += productArry[i].price;
   }
 }
